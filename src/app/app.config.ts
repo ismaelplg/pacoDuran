ed71920fe510a113945f67297c8d9a0c2c9e5644
Ismael Lopez <ismaelplg@hotmail.com>
2025-07-08 09:10:44 -0600

    chore: add default angular animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withHashLocation,
+  withViewTransitions,
+} from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withViewTransitions({ skipInitialTransition: false })
+    ),
+    provideAnimationsAsync(),
+  ],
 };
