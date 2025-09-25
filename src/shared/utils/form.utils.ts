e6a712c9f5a1b3ccc991ab17c966b2e0100b943f
Ismael Lopez <ismaelplg@hotmail.com>
2025-09-25 15:11:19 -0600

    feat: add utils and type

---
+++ src/shared/utils/form.utils.ts
@@ -0,0 +1,39 @@
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class FormUtils {
+  // Expresiones regulares/
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
+  static getTextError(errors: ValidationErrors) {
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} carácteres.`;
+
+        case 'email':
+          return 'Ingresa un correo válido';
+
+        default:
+          return `Error de validación no controlado ${key}`;
+      }
+    }
+
+    return null;
+  }
+
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return !!form.controls[fieldName].errors && form.controls[fieldName].touched;
+  }
+
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+
+    const errors = form.controls[fieldName].errors ?? {};
+
+    return FormUtils.getTextError(errors);
+  }
+}
